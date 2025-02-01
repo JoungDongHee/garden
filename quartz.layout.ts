b37c441a9060b85273b1a3ce721237db6d9e3db5
JDH <jdh@sncit.net>
2025-02-01 18:47:44 +0900

    Quartz sync: Feb 1, 2025, 6:47 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -5,7 +5,21 @@ import * as Component from "./quartz/components"
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
   header: [],
-  afterBody: [],
+  afterBody: [
+    Component.Comments({
+      provider: 'giscus',
+      options: {
+        // from data-repo
+        repo: 'JoungDongHee/garden',
+        // from data-repo-id
+        repoId: 'R_kgDOM1m9pg',
+        // from data-category
+        category: 'Announcements',
+        // from data-category-id
+        categoryId: 'DIC_kwDOM1m9ps4CmiFK',
+      }
+    }),
+  ],
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
